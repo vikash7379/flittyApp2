91168144dd279f1ff6a64d6c1b6179190908e834
Vikash Chaurasia <Jaichaurasia90@gmail.com>
2022-12-28 17:55:25 +0530

    home page final

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+let docTitile = document.title;
+window.addEventListener('blur',()=>{
+    document.title = "Come Back 🙀 | Flitty"
+})
+window.addEventListener('focus', ()=>{
+    document.title = docTitile
+})
 
 
+let tc = document.querySelector("#toggleClass");
+let line1 = document.querySelector("#toggleBar1");
+let line2 = document.querySelector("#toggleBar2");
+let line3 = document.querySelector("#toggleBar3");
+let lines = document.querySelector("#toggleBar");
 
-function openAccor () {
-    alert("hi")
+
+function mobMenu () {
+    tc.classList.toggle("dispNav");
+    lines.classList.toggle("hm-click");
+    line1.classList.toggle("line1-click");
+    line2.classList.toggle("line2-click");
+    line3.classList.toggle("line3-click");
+    container.classList.toggle("fixedBgMob")
 }
-
-console.log("i am working")
