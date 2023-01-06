761da6d3451f6fc1821f49d5c506239d3ad1cdf2
Rajat Verma <96742104+Rajat-eng@users.noreply.github.com>
2023-01-06 15:20:43 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router=express.Router();
 
-const {register}=require('../controllers/user');
+const {reportAbuse,contact}=require('../controllers/user');
 
-router.route('/register').post(register);
+router.route('/reportAbuse').post(reportAbuse);
+router.route('/contact').post(contact);
 
 module.exports=router;
